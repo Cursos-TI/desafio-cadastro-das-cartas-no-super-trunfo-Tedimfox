86d36bbcecd437ab1a42405aeaecf289712a94e4
Tedimfox <pedrohnsw1@gmail.com>
2025-02-12 00:57:37 +0000

    test aventureiro, eu criei o printf

--- Cartas/CartasSuperTrunfo.c
+++ Cartas/CartasSuperTrunfo.c
@@ -2,42 +2,47 @@
 
     int main(){
 
-        int cidade;
+        
         char estado;
         char pais;
+
+        char cidade = 'A';
         char codigo;
-        char nome[50];
-        int area;
-        int populacao;
+        float area;
+        float populacao;
         int pontos;
-        int pib;
-        char opcao1 = '1';
-        char opcao2 = '2';
-        char opcao3 = '3';
-        char opcao4 = '4';
+        float pib;
+        float densidade;
+        float percapta;
+
+
+        int opcao1 = 1;
+        int opcao2 = 2;
+        int opcao3 = 3;
+        int opcao4 = 4;
 
         printf("Super Trunfo! (PAÍSES) \n ");
 
         printf("Escolha um País: \n");
         scanf("%s", &pais);
 
-        printf("Escolha um Estado: \n");
-        scanf(" %c", &estado);
+        printf("Escolha um Estado(*SIGLA*): \n");
+        scanf(" %s", &estado);
 
         printf("Escolha uma Cidade: \n");
-        scanf("%d", &cidade );
+        scanf("%s", &cidade );
 
             printf("Digite o Código (a combinacão do Estado com a Cidade): \n");
-            scanf("%s", &codigo);       
+            scanf(" %s", &codigo);       
            
             printf("Digite a População Total: \n");
-            scanf(" %d", &populacao);
+            scanf(" %f", &populacao);
 
             printf("Digite a Área: \n");
-            scanf("%d", &area);
+            scanf("%f", &area);
             
             printf("Digite o PIB: \n");
-            scanf(" %d", &pib);
+            scanf(" %f", &pib);
             
             printf("Digite os Pontos Turisticos: \n");
             scanf(" %d", &pontos);
@@ -45,118 +50,21 @@
 printf("\n");
             
 
-            printf("Carta: %c  \n" ,opcao1);
+            printf("Carta:0%d\n" , opcao1);
             
 printf("\n");
-
-            printf("Populacão: %d\n", populacao);
-            printf("Área: %d\n ", area);
-            printf("PIB: %d\n ", pib);
+            printf("Código:A0%d\n", opcao1);
+            printf("Populacão: %.0f\n", populacao);
+            printf("Área: %.0fKM²\n ", area);
+            printf("PIB:R$%.0f\n ", pib);
             printf("Pontos Turísticos: %d\n", pontos);
+
+            densidade = (float)(populacao / area);
+            percapta = (float)(pib / populacao);
+
+            printf("Densidade Populacional:%.0f Habitante por M²\n", densidade);
+            printf("PIB per CAPTA:%.3f\n", percapta);
 printf("\n");
-printf("\n");
-            
-
-
-
- printf("Escolha uma Cidade: \n");
-        scanf("%d", &cidade );
-
-            printf("Digite o Código (a combinacão do Estado com a Cidade): \n");
-            scanf("%s", &codigo);       
-           
-            printf("Digite a População Total: \n");
-            scanf(" %d", &populacao);
-
-            printf("Digite a Área: \n");
-            scanf("%d", &area);
-            
-            printf("Digite o PIB: \n");
-            scanf(" %d", &pib);
-            
-            printf("Digite os Pontos Turisticos: \n");
-            scanf(" %d", &pontos);
-
-printf("\n");
-            
-
-            printf("Carta: %c  \n" ,opcao2);
-            
-printf("\n");
-
-            printf("Populacão: %d\n", populacao);
-            printf("Área: %d\n ", area);
-            printf("PIB: %d\n ", pib);
-            printf("Pontos Turísticos: %d\n", pontos);
-printf("\n");
-printf("\n");
-
-
-
- printf("Escolha uma Cidade: \n");
-        scanf("%d", &cidade );
-
-            printf("Digite o Código (a combinacão do Estado com a Cidade): \n");
-            scanf("%s", &codigo);       
-           
-            printf("Digite a População Total: \n");
-            scanf(" %d", &populacao);
-
-            printf("Digite a Área: \n");
-            scanf("%d", &area);
-            
-            printf("Digite o PIB: \n");
-            scanf(" %d", &pib);
-            
-            printf("Digite os Pontos Turisticos: \n");
-            scanf(" %d", &pontos);
-
-printf("\n");
-            
-
-            printf("Carta: %c  \n" ,opcao3);
-            
-printf("\n");
-
-            printf("Populacão: %d\n", populacao);
-            printf("Área: %d\n ", area);
-            printf("PIB: %d\n ", pib);
-            printf("Pontos Turísticos: %d\n", pontos);
-printf("\n");
-printf("\n");
-
-
-
-
- printf("Escolha uma Cidade: \n");
-        scanf("%d", &cidade );
-
-            printf("Digite o Código (a combinacão do Estado com a Cidade): \n");
-            scanf("%s", &codigo);       
-           
-            printf("Digite a População Total: \n");
-            scanf(" %d", &populacao);
-
-            printf("Digite a Área: \n");
-            scanf("%d", &area);
-            
-            printf("Digite o PIB: \n");
-            scanf(" %d", &pib);
-            
-            printf("Digite os Pontos Turisticos: \n");
-            scanf(" %d", &pontos);
-
-printf("\n");
-            
-
-            printf("Carta: %c \n" ,opcao4);
-            
-printf("\n");
-
-            printf("Populacão: %d\n", populacao);
-            printf("Área: %d\n ", area);
-            printf("PIB: %d\n ", pib);
-            printf("Pontos Turísticos: %d\n", pontos);
 
 return 0;
     }
